f368c049ce456401f9b99f142c2b56ff56cdc041
liubowen <banhujiu@gmail.com>
2018-12-14 17:36:30 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	// 初始化引擎
+	route := gin.Default()
+
+	// 注册一个路由和处理函数
+	route.GET("/", WebRoot)
+	// 绑定端口，然后启动应用
+	route.Run(":80")
+}
+
+/**
+* 根请求处理函数
+* 所有本次请求相关的方法都在 context 中，完美
+* 输出响应 hello, world
+*/
+func WebRoot(context *gin.Context) {
+	context.String(http.StatusOK, "hello, world")
+}
